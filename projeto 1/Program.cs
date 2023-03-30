fc1bb66c9861d10d8581e0661f2a7ca06a5b01af
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-03-30 10:56:23 -0300

    projeto 1

---
+++ projeto 1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine($"Digite o salario recebido");
+ float salario = float.Parse (Console.ReadLine ());
+
+ Console.WriteLine($"digite o total do gasto");
+ float gasto = float.Parse (Console.ReadLine ());
+
+
+ if  (gasto > salario);
+ {
+    Console.WriteLine($"Orçamento estourado");
+    
+ }
+
+ else
+  
+ {
+    Console.WriteLine($"gasto dentro do orçamento");
+ 
+ }
+ 
+
