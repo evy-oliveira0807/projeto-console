af0d0e9043688ab524bc0d032c795b68aa2726a2
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-03-31 10:33:15 -0300

    projeto 4

---
+++ projeto 4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine($"Digite a senha:");
+float senhadigitada = float.Parse ( Console.ReadLine ());
+
+int senhacorreta = 1234;
+
+if (senhadigitada == senhacorreta)
+{
+    Console.WriteLine($"Permitido");
+    
+}
+else
+{
+    Console.WriteLine($"Negado");
+    
+}
