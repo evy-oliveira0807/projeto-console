bebcbefb1ff3a933707793b475c0b0e3d2708338
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-03-31 10:12:02 -0300

    projeto 3

---
+++ projeto 3/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine($"digita a medidas de lado A: ");
+string ladoA =  Console.ReadLine ();
+Console.WriteLine($"digita a medidas de lado B: ");
+string ladoB =  Console.ReadLine ();
+Console.WriteLine($"digita a medidas de lado C: ");
+string ladoC =  Console.ReadLine ();
+
+
+
+if ( (ladoA == ladoB) && ( ladoB == ladoC))
+{
+ Console.WriteLine($"equilatero");
+ 
+}
+  else if ((ladoA == ladoB) || (ladoB == ladoC) )
+  {
+    Console.WriteLine($" Isóscele");
+    
+  }
+
+  else if ((ladoA != ladoB) &&  (ladoB!= ladoC) )
+  {
+    Console.WriteLine($"Escaleno");
+    
+  }
+
+
+
+
+
+
