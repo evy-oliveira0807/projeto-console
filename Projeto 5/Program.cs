cc38d7c5bc7398ba08a025e941978e562d067851
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-03-31 11:02:32 -0300

    projeto 5

---
+++ Projeto 5/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine($"Digite o numero de maças compradas");
+float maças = float.Parse (Console.ReadLine( ));
+
+float macasduzia = 0.25f;
+float macasonze = 0.30f;
+
+if (maças >= 12)
+{
+    float valor= maças * macasduzia; 
+ Console.WriteLine($"{valor}");
+ 
+}
+
+else
+{
+    float valor= maças *  macasonze;
+    Console.WriteLine($"{valor}");
+}
+
