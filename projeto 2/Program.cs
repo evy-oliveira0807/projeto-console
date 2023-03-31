a3baaee9d3999786d8865151f31e6d69854166ec
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-03-31 08:48:41 -0300

    projeto 2

---
+++ projeto 2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine($" Quantos gol fez o time A");
+float timeA = float.Parse (Console.ReadLine());                    
+
+Console.WriteLine($" Quantos gol fez o time B");
+float timeB = float.Parse (Console.ReadLine());  
+
+if (timeA < timeB)
+{
+
+    Console.WriteLine($"Time B ganhou");
+    
+}
+
+else
+{
+    Console.WriteLine($"Time A ganhou");
+    
+
+}
+
+if ( timeA == timeB)
+
+{
+ Console.WriteLine($"Deu empate");
+ 
+}
