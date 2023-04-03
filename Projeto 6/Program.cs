8cbad7c8c434fa1c89dea1225b28af3a4d4663b2
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-04-03 09:24:16 -0300

    projeto 6

---
+++ Projeto 6/Program.cs
@@ -0,0 +1,29 @@
+﻿
+float media;
+float frequencia;
+
+Console.WriteLine($"Entre com a nota media do aluno (%): ");
+media = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Entre com a frequencia do aluno (%): ");
+ frequencia = float.Parse(Console.ReadLine());
+
+ if( media >= 7.0F && (frequencia/100) >= 0.75F)
+ {
+    Console.WriteLine($"Aluno aprovado!");
+
+ }
+
+ else if ((media > 3.0F && media <7.0F) && (frequencia/100) >= 0.75F)
+{
+    Console.WriteLine($"aluno em recuperaçao!");
+    
+}
+else
+{
+    Console.WriteLine($"Aluno reprovado!");
+    
+}
+
+
+
