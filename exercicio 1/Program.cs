6ef00a5867efdfcfc97d216ffef7c39472e097c8
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-04-06 08:45:38 -0300

    exercicio 1

---
+++ exercicio 1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+    Console.WriteLine($"digite uma nota entre 0 e 10:");
+    int nota  = int.Parse(Console.ReadLine());
+
+  while (nota < 0 || nota > 10)
+  {
+    Console.WriteLine($"Nota invalida:");
+    Console.WriteLine($"Digite apenas notas entre 0 e 10:");
+     nota  = int.Parse(Console.ReadLine());
+    
+  }
+
+  Console.WriteLine($"oba, vc digitou a nota correta, obgd até mais.");
+
+  
+
+
+
+
+    
+   
+    
