1c8861fd57224c2cbbf0dad656b68dfd6c88b195
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-04-06 10:17:32 -0300

    exercicios 2

---
+++ exercicio 2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine($"Digite seu nome:");
+string nome = (Console.ReadLine());
+
+Console.WriteLine($"Digite sua senha:");
+string senha = (Console.ReadLine());
+
+ while (senha == nome)
+{
+     Console.WriteLine($"Senha ivalida:");
+     Console.WriteLine($"Digite uma senha que nao pareça com seu nome:");
+      senha = (Console.ReadLine());
+
+
+    
+ }
+
+Console.WriteLine($"Senha valida.");
+
