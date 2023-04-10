637beeae977bb573260f23fcc1bacd3c5c85b7d7
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-04-10 10:59:34 -0300

    exercicios 5

--- exercicios 3/Program.cs
+++ exercicios 3/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Faça um programa que receba um número inteiro e mostre a sua tabuada.
+
+Console.WriteLine($"Qual tabuada vc quer saber?");
+
+int tabuada = int.Parse() (Console.ReadLine());
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{tabuada} x {i} = {tabuada * i}");
+    
+}
+
+Console.WriteLine($"1 X 5 = 1");
+
+Console.WriteLine($"2 X 5 = 2 ");
+
+Console.WriteLine($"3 X 5 = 3");
+
+Console.WriteLine($"4 X 5 = 4");
+
+Console.WriteLine($"5 X 5 = 5");
+
+Console.WriteLine($"6 X 5 = 6");
+
+Console.WriteLine($"7 X 5 = 7");
+
+Console.WriteLine($"8 X 5 = 8");
+
+Console.WriteLine($"9 X 5  9");
+
+Console.WriteLine($"10 X 5 = 10");
+
+
+
+
+
+
+
