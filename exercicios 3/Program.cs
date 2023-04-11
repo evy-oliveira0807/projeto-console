0ab97af9cbf721839bfcbba6fcd5f06862c86cf3
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-04-11 08:09:57 -0300

    exercicios 3

--- exercicios 3/Program.cs
+++ exercicios 3/Program.cs
@@ -2,7 +2,7 @@
 
 Console.WriteLine($"Qual tabuada vc quer saber?");
 
-int tabuada = int.Parse() (Console.ReadLine());
+int tabuada = int.Parse(Console.ReadLine());
 
 for (int i = 1; i <= 10; i++)
 {
@@ -10,25 +10,7 @@ for (int i = 1; i <= 10; i++)
     
 }
 
-Console.WriteLine($"1 X 5 = 1");
 
-Console.WriteLine($"2 X 5 = 2 ");
-
-Console.WriteLine($"3 X 5 = 3");
-
-Console.WriteLine($"4 X 5 = 4");
-
-Console.WriteLine($"5 X 5 = 5");
-
-Console.WriteLine($"6 X 5 = 6");
-
-Console.WriteLine($"7 X 5 = 7");
-
-Console.WriteLine($"8 X 5 = 8");
-
-Console.WriteLine($"9 X 5  9");
-
-Console.WriteLine($"10 X 5 = 10");
 
 
 
