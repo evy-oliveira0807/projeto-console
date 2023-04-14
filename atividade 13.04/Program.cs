7a78233fac4d8a9fd8f65c4c25541741d82337a8
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-04-14 10:59:33 -0300

    atividade 13.04

---
+++ atividade 13.04/Program.cs
@@ -0,0 +1,39 @@
+﻿// Ler o ano atual e o ano de nascimento de uma pessoa. Escrever uma mensagem que diga se
+// ela poderá ou não votar este ano (não é necessário considerar o mês em que a pessoa nasceu).
+
+
+// //DECLARACAO DE VARIAVEL 
+// int anoAtual, nascimento, idade;
+
+// //INICIALIZACAO DE UMA VAREALVEL
+//  anoAtual = 1; 
+//  nascimento = 10;
+//  anoAtual = 20;
+//  nascimento = 60;
+//  anoAtual = 5;
+//  idade = 0;
+// //PROCESSAMENTO
+
+//  if(false)
+//  {
+//  idade = anoAtual + nascimento;
+
+//  }
+// //SAIDA
+// /* idade = 120; */
+// Console.WriteLine($"A sua idade é {idade}:");
+
+
+//declaracao de variavel
+int anoAtual, nascimento, idade;
+
+//inicializacao de uma variavel
+
+anoAtual=2023;
+nascimento= 2005;
+
+// processamento
+
+
+
+
