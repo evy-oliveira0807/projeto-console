38807cdbe7d06710ae6ea6657ecd210bff7b4a3f
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-04-19 09:24:37 -0300

    exercicio 7 13.04

---
+++ exercicios7 13.04/Program.cs
@@ -0,0 +1,8 @@
+﻿//Faça um algoritmo para ler 15 números e armazenar em um vetor. Após a leitura total dos 15 números, o algoritmo deve escrever esses 15 números lidos na ordem inversa da qual foi
+// // declarado.
+
+ int[] numeros = { 10, 12, 7, 1, 4 };
+ Array.Sort(numeros);
+ foreach (var n in numeros){
+     Console.Write($"{n} ");
+ }
