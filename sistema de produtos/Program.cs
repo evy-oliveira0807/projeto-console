4334993060e101578eba70b5559e742bf20820dc
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-05-25 08:26:56 -0300

    produto

---
+++ sistema de produtos/Program.cs
@@ -0,0 +1,78 @@
+﻿
+int count = 3;
+char opcao;
+string[] produtos = new string[count];
+float[] preco = new float[count];
+bool[] promocao = new bool[count];
+string resposta;
+
+static bool validarPromo()
+{
+    Console.WriteLine($"o produto está em promoção?");
+    string resposta = Console.ReadLine();
+    
+    if (resposta == "sim")
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+static void cadastro(string[] nome,float[] preco, bool[] promocao)
+{
+    for (int i = 0; i < nome.Length; i++)
+    {
+     Console.WriteLine($"Produto:");
+     nome[i]= Console.ReadLine();
+
+     Console.WriteLine($"Preço:");
+     preco[i]= float.Parse (Console.ReadLine());    
+
+     promocao[i]= validarPromo();     
+    }  
+}
+do
+{
+    Console.WriteLine(@$"
+
+********************************
+*  Escolha uma opção do menu:  *
+*                              *
+*  1-cadrastrar produto        *
+*  2-Listar produto            *
+*  3-mostrar menu              *
+********************************
+");
+
+    opcao = char.Parse(Console.ReadLine());
+    switch (opcao)
+    {
+        case '1':
+            cadastro(produtos,preco,promocao);
+            break;
+        case '2':
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"------------------------");
+                
+                Console.WriteLine($"Produto:{produtos[i]}");
+                Console.WriteLine($"preço:{preco[i]}");
+                if (promocao[i] == true)
+                {
+                    Console.WriteLine($"**o produto está em promoção**");
+                }
+                else
+                {
+                    Console.WriteLine($"");
+                    
+                }        
+            }
+            break;
+        default:
+            break;
+    }
+
+} while (opcao != 3);
+
